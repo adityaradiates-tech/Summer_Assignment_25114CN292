d765021169900fabcb13afeb321070ece5620489
Aditya Raj Yadav <adityaradiates@gmail.com>
2026-07-07 21:12:09 +0530

    Add files via upload
    day_23 summer assignment

---
+++ day_23/program_3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+int main()
+{
+    char str[100],str2[100];
+    int i,j,len1=0,len2=0,temp;
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+    
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    while(str1[len1]!='\0')
+      len1++;
+    
+
+    while(str2[len2]!='\0')
+     len2++;
+
+     if(len1!=len2)
+     {
+         printf("Strings are not anagrams.\n");
+         return 0;
+     }
+     {
+        printf("Strings are not anagrams.\n");
+        return 0;
+     }
+     for(i=0; i<len1-1; i++)
+     {
+         for(j=i+1; j<len1; j++)
+         {
+             if(str1[i]>str1[j])
+             {
+                 temp=str1[i];
+                 str1[i]=str1[j];
+                 str1[j]=temp;
+             }
+             if(str2[i]>str2[j])
+             {
+                 temp=str2[i];
+                 str2[i]=str2[j];
+                 str2[j]=temp;
+             }
+         }
+     }
+     printf("Strings are anagrams.\n");
+     return 0;
+}
